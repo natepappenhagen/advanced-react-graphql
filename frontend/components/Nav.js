0a14ccdd70538fc6d220999b6b9408c85a86f147
Nate Pappenhagen <natepappenhagen@gmail.com>
2019-01-20 14:45:43 -0700

    added User and Nav components and updated the Query resolvers and schema to reflect users

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -2,13 +2,21 @@
  * @Author: Nate
  * @Date:   2019-01-11 14:12:12
  * @Last Modified by:   Nate
- * @Last Modified time: 2019-01-13 14:38:43
+ * @Last Modified time: 2019-01-20 14:39:19
  */
 import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
+import User from "./User";
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({ data: { me } }) => {
+        console.log(me);
+        if (me) return <p>{me.name}</p>;
+        return null;
+      }}
+    </User>
     <Link href="/items">
       <a>Shop</a>
     </Link>
